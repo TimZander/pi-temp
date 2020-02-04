00136867f7fa20adccfbb0f3bab8706bbf000ac0
Tim Zander <tzander@breckgv.com>
2020-02-04 10:56:39 -0700

    pymssql syntax fix

--- src/data_storage.py
+++ src/data_storage.py
@@ -21,6 +21,6 @@ def save_to_sql(connection_string, temperature_reading):
 def save_to_sql_pymssql(server, user, password, database, temperature_reading):
     connection = connect_pymssql(server, user, password, database)
     cursor = connection.cursor()
-    cursor.execute('INSERT INTO temperature.dbo.readings (probe_serial, reading_date, temperature_value) VALUES (%s, %s, %s)', [(temperature_reading.serial, temperature_reading.datetime, temperature_reading.temperature_celcius)])
-    cursor.commit()
+    cursor.execute('INSERT INTO temperature.dbo.readings (probe_serial, reading_date, temperature_value) VALUES (%s, %s, %s)', (temperature_reading.serial, temperature_reading.datetime, temperature_reading.temperature_celcius))
+    connection.commit()
     connection.close()
