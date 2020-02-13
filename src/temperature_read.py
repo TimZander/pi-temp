5755deb75a4ea0c3e43105b6ed7f4d797440c83a
Tim Zander <tim.zander@gmail.com>
2020-02-12 19:28:07 -0700

    change time to use UTC (#26)

--- src/temperature_read.py
+++ src/temperature_read.py
@@ -24,4 +24,4 @@ def read_temp(device):
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         # temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return TemperatureReading(device.serial, temp_c, datetime.now())
+        return TemperatureReading(device.serial, temp_c, datetime.utcnow())
